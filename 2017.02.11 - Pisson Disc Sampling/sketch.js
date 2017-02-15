c2e7edb89aef738d69b6374783816c9154c62ec6
Lyubomir Slavilov <lyubo.slavilov@gmail.com>
2017-02-15 11:42:38 +0200

    White space cleanup

--- 2017.02.11 - Pisson Disc Sampling/sketch.js
+++ 2017.02.11 - Pisson Disc Sampling/sketch.js
@@ -1,9 +1,9 @@
 var poisson;
 
 function setup() {
-	createCanvas(800, 800);
-	background(0);
-	//Create the poisson disc sampler
+    createCanvas(800, 800);
+    background(0);
+    //Create the poisson disc sampler
     poisson = new Poisson(20, 30);
 }
 
@@ -11,9 +11,9 @@ function setup() {
 function draw() {
     background(0);
 
-	//Run the sampling process once
+    //Run the sampling process once
     poisson.run();
-	//Draw it
+    //Draw it
     poisson.show();
 
 }
