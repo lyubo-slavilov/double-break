ed02bcca29962c1f7e27b957ae8d6d98b86c07c1
Lyubomir Slavilov <lyubo.slavilov@gmail.com>
2017-03-07 13:25:00 +0200

    Adding some sliders

--- 03 Flocking Systems/05 Cohesion/sketch.js
+++ 03 Flocking Systems/05 Cohesion/sketch.js
@@ -1,10 +1,24 @@
 var birds = [];
+var sepSlider, algnSliver, cohSlider;
+
 function setup() {
     createCanvas(800,600);
 
     var bird = new Bird(random(width), random(height), 20);
     bird.v.set(random(-3,3), random(-3,3));
     birds.push(bird);
+    
+    var div = createDiv('Separate: ');
+    sepSlider = createSlider(0, 5, 1.5, 0.1); //Slider from 0 to 5 with current value of 1.5 and step size of 0.1
+    sepSlider.parent(div);
+    
+    var div = createDiv('Align: ');
+    algnSlider = createSlider(0, 5, 1, 0.1); //Slider from 0 to 5 with current value of 1 and step size of 0.1
+    algnSlider.parent(div);
+    
+    var div = createDiv('Cohesion: ');
+    cohSlider = createSlider(0, 5, 1.5, 0.1); //Slider from 0 to 5 with current value of 1 and step size of 0.1
+    cohSlider.parent(div);
 }
 
 function keyPressed() {
@@ -20,9 +34,9 @@ function keyPressed() {
 function draw() {
     background(53);
     for (var i = 0; i < birds.length; i++) {
-        birds[i].separate(birds, 1.5);
-        birds[i].cohesion(birds, 1);
-        birds[i].align(birds, 1);
+        birds[i].separate(birds, sepSlider.value());
+        birds[i].cohesion(birds, cohSlider.value());
+        birds[i].align(birds, algnSlider.value());
         birds[i].update();
         birds[i].show();
     }
