c1663e791d4febc42b0c03afde0fae7cf9b51823
Lyubomir Slavilov <lyubo.slavilov@gmail.com>
2017-06-21 01:42:05 +0300

    Forgotten comment line added

--- 09 Futiristic graph effect/particle.js
+++ 09 Futiristic graph effect/particle.js
@@ -18,7 +18,7 @@ function Particle(seed) {
 
         var wx, wy;
 
-        //Play with this number to produce different wiggle behaviours
+        //Play with this numbers to produce different wiggle behaviours
         var wiggle = 300;
         var wiggleScale = 1/50;
 
